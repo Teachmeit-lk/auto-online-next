3868c75f202602cfc462a40439051cc3b6ceb384
MadurangaDev <shehan@teachmeit.lk>
2025-01-09 08:48:35 +0530

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const Header: FC = () => {
+  return (
+    <header className="flex justify-center items-center gap-4">
+      <h1 className="text-3xl font-bold">Hello, World!</h1>
+    </header>
+  );
+};
