a121f1a1856290c33490e8dcab99e2cfab9e6f49
sajithfdo <165661446+SajithPrawanthaFernando@users.noreply.github.com>
2025-01-10 11:28:44 +0530

    Implemented the  Footer Component
    ## Features
    - Implemented the Desktop Footer Component

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,8 @@
+//Main Components
+export { default as Footer } from "@/components/Footer";
+
+//Svg Components
+export { default as FacebookIcon } from "@/assets/FacebookIcon";
+export { default as InstagramIcon } from "@/assets/InstagramIcon";
+export { default as TwitterIcon } from "@/assets/TwitterIcon";
+export { default as YoutubeIcon } from "@/assets/YoutubeIcon";
