b2ccd04f87f407620accfd0cf2a30ab750e6fa65
sajithfdo <165661446+SajithPrawanthaFernando@users.noreply.github.com>
2025-01-30 16:55:58 +0530

    Implemented View Completed Order Modal
    ## Features
    - Implemented View Completed Order Modal desktop Version

---
+++ src/app/modal/ViewCompletedOrderModal.tsx
@@ -0,0 +1,185 @@
+"use client";
+
+import * as Dialog from "@radix-ui/react-dialog";
+import { CirclePlus } from "lucide-react";
+import Image from "next/image";
+
+import { ViewEstimate1 } from "@/app//assets/images";
+
+interface EstimateModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const ViewCompletedOrderModal: React.FC<EstimateModalProps> = ({
+  isOpen,
+  onClose,
+}) => {
+  return (
+    <Dialog.Root open={isOpen} onOpenChange={onClose}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-black/40 backdrop-blur-none" />
+        <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[700px] bg-white py-8 px-7 rounded-[10px] shadow-lg focus:outline-none">
+          <Dialog.Title className="text-[15px] font-bold mb-5 text-[#111102] font-body text-left">
+            View Completed Order
+          </Dialog.Title>
+
+          {/* Gray Container */}
+          <div className="bg-[#F8F8F8] rounded-[8px] p-8 space-y-6">
+            {/* Image Section */}
+            <div className="flex justify-center">
+              <Image
+                src={ViewEstimate1}
+                alt="Vehicle Image"
+                className="w-[107px] h-[73px]   rounded-[3px] object-cover"
+              />
+            </div>
+
+            {/* Form Section */}
+            <form className="grid grid-cols-3 gap-y-4 gap-x-6">
+              {/*  Booked ID */}
+              <div>
+                <label className="text-[12px] font-body font-[500] text-[#111102]">
+                  Booked ID
+                </label>
+                <input
+                  type="text"
+                  placeholder="BID6584"
+                  readOnly
+                  className="w-full h-[36px] placeholder:text-[#111102] text-[#111102] font-body text-[10px] mt-1 px-3 bg-[#FEFEFE] rounded-[3px] focus:outline-none focus:ring-2 focus:ring-[#F9C301]"
+                />
+              </div>
+
+              {/* Vendor Code */}
+              <div>
+                <label className="text-[12px] font-body font-[500] text-[#111102]">
+                  Vendor Code
+                </label>
+                <input
+                  type="text"
+                  placeholder="VC6574"
+                  readOnly
+                  className="w-full h-[36px] placeholder:text-[#111102] text-[#111102] font-body text-[10px] mt-1 px-3 bg-[#FEFEFE] rounded-[3px] focus:outline-none focus:ring-2 focus:ring-[#F9C301]"
+                />
+              </div>
+
+              {/* Part Name */}
+              <div>
+                <label className="text-[12px] font-body font-[500] text-[#111102]">
+                  Part Name
+                </label>
+                <input
+                  type="text"
+                  placeholder="Tyres"
+                  readOnly
+                  className="w-full h-[36px] placeholder:text-[#111102] text-[#111102] font-body text-[10px] mt-1 px-3 bg-[#FEFEFE] rounded-[3px] focus:outline-none focus:ring-2 focus:ring-[#F9C301]"
+                />
+              </div>
+
+              {/* Booked Date */}
+              <div>
+                <label className="text-[12px] font-body font-[500] text-[#111102]">
+                  Booked Date
+                </label>
+                <input
+                  type="text"
+                  placeholder="August 19, 2024"
+                  readOnly
+                  className="w-full h-[36px] placeholder:text-[#111102] text-[#111102] font-body text-[10px] mt-1 px-3 bg-[#FEFEFE] rounded-[3px] focus:outline-none focus:ring-2 focus:ring-[#F9C301]"
+                />
+              </div>
+
+              {/* Order Completion Date */}
+              <div>
+                <label className="text-[12px] font-body font-[500] text-[#111102]">
+                  Order Completion Date
+                </label>
+                <input
+                  type="text"
+                  placeholder="August 24, 2024"
+                  readOnly
+                  className="w-full h-[36px] placeholder:text-[#111102] text-[#111102] font-body text-[10px] mt-1 px-3 bg-[#FEFEFE] rounded-[3px] focus:outline-none focus:ring-2 focus:ring-[#F9C301]"
+                />
+              </div>
+
+              {/* Contact Number */}
+              <div>
+                <label className="text-[12px] font-body font-[500] text-[#111102]">
+                  Contact Number
+                </label>
+                <input
+                  type="text"
+                  placeholder="0715263521"
+                  readOnly
+                  className="w-full h-[36px] placeholder:text-[#111102] text-[#111102] font-body text-[10px] mt-1 px-3 bg-[#FEFEFE] rounded-[3px] focus:outline-none focus:ring-2 focus:ring-[#F9C301]"
+                />
+              </div>
+
+              {/* Item Count */}
+              <div>
+                <label className="text-[12px] font-body font-[500] text-[#111102]">
+                  Item Count
+                </label>
+                <input
+                  type="text"
+                  placeholder="2"
+                  readOnly
+                  className="w-full h-[36px] placeholder:text-[#111102] text-[#111102] font-body text-[10px] mt-1 px-3 bg-[#FEFEFE] rounded-[3px] focus:outline-none focus:ring-2 focus:ring-[#F9C301]"
+                />
+              </div>
+
+              {/* Total Price */}
+              <div>
+                <label className="text-[12px] font-body font-[500] text-[#111102]">
+                  Total Price (Rs.)
+                </label>
+                <input
+                  type="text"
+                  placeholder="25678.00"
+                  readOnly
+                  className="w-full h-[36px] placeholder:text-[#111102] text-[#111102] font-body text-[10px] mt-1 px-3 bg-[#FEFEFE] rounded-[3px] focus:outline-none focus:ring-2 focus:ring-[#F9C301]"
+                />
+              </div>
+
+              {/* Net Total Price */}
+              <div>
+                <label className="text-[12px] font-body font-[500] text-[#111102]">
+                  Net Total Price (Rs.)
+                </label>
+                <input
+                  type="text"
+                  placeholder="28937.00"
+                  readOnly
+                  className="w-full h-[36px] placeholder:text-[#111102] text-[#111102] font-body text-[10px] mt-1 px-3 bg-[#FEFEFE] rounded-[3px] focus:outline-none focus:ring-2 focus:ring-[#F9C301]"
+                />
+              </div>
+
+              {/* Description */}
+              <div className="col-span-3">
+                <label className="text-[12px] font-body font-[500] text-[#111102]">
+                  Description
+                </label>
+                <textarea
+                  rows={3}
+                  readOnly
+                  placeholder="Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+                  className="w-full placeholder:text-[#111102] h-[80px] mt-1 p-3 text-[10px] text-body bg-[#FEFEFE] rounded-[3px] text-[#111102] focus:outline-none focus:ring-2 focus:ring-[#F9C301]"
+                />
+              </div>
+            </form>
+          </div>
+
+          {/* Close Button */}
+          <Dialog.Close asChild>
+            <button
+              onClick={onClose}
+              className="absolute top-4 right-4 text-gray-500 hover:text-[#F9C301]"
+            >
+              <CirclePlus className="rotate-45" />
+            </button>
+          </Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
