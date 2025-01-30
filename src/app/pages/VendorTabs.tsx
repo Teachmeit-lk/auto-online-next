958448647f3bd833bc934210795a1b3d92c00df0
sajithfdo <165661446+SajithPrawanthaFernando@users.noreply.github.com>
2025-01-30 14:34:17 +0530

    Implemented Vendor Layout
    ## Features
    - Implemented vendor layout desktop version.

---
+++ src/app/pages/VendorTabs.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+// import {
+//   VendorProducts,
+//   NewPriceRequests,
+//   NewPurchaseOrders,
+//   VendorGallery,
+//   CompletedOrders,
+// } from "@/components/vendor";
+// import { VendorLayout } from "@/app/layout/VendorLayout";
+
+export const VendorTabs: React.FC = () => {
+  // const tabs = [
+  //   // { label: "Vendor Products", component: <VendorProducts /> },
+  //   // { label: "Completed Orders", component: <CompletedOrders /> },
+  //   // { label: "New Price Requests", component: <NewPriceRequests /> },
+  //   // { label: "New Purchase Orders", component: <NewPurchaseOrders /> },
+  //   // { label: "Vendor Gallery", component: <VendorGallery /> },
+  // ];
+
+  return (
+    <div className="container mx-auto" id="tabspage">
+      {/* <VendorLayout tabs={tabs} /> */}
+    </div>
+  );
+};
