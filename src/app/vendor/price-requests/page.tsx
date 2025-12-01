a460e8bcc4aba87fa3cd69ac0c1f5cabb66db5f3
sajithfdo <sajith@teachmeit.lk>
2025-12-01 14:06:22 +0530

    Added Completed New Price Requests vendor buyer WhatsApp messaging functionality.
    ## Fixes
    - Added Completed New Price Requests vendor buyer WhatsApp messaging functionality.

--- src/app/vendor/price-requests/page.tsx
+++ src/app/vendor/price-requests/page.tsx
@@ -375,7 +375,10 @@ const NewPriceRequests: React.FC = () => {
                           </button>
                           <button
                             className="bg-[#D1D1D1] py-3 text-[#111102] text-[12px] w-full h-full focus:hover:bg-yellow-500 hover:bg-yellow-500"
-                            // onClick={() => setIsModalOpen(true)}
+                            onClick={() => {
+                              setSelectedRequest(vendor.raw);
+                              setIsModalOpen(true);
+                            }}
                           >
                             Chat
                           </button>
